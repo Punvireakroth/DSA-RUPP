42122d677c3317c9137dafdd9cf42273835d4212
Punvireakroth <vireakrothpun@gmail.com>
2023-03-18 19:43:31 +0700

    intial commit

---
+++ QuickSort/recursive_quicksort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+// Partition function for QuickSort algorithm
+
+int partition(int arr[], int low, int high)
+{
+    // Select the last element as the pivot
+    int pivot = arr[high];
+
+    int i = low - 1;
+
+    // Traverse the array
+    for (int j = low; j <= high - 1; j++)
+    {
+        // If the current element is smaller than or equal to the pivot
+        if (arr[j] <= pivot)
+        {
+            // Increment index of smaller element
+            i++;
+            // Swap the current element with the element at index i
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    // Swap the pivot element with the element at index i+1
+    swap(arr[i + 1], arr[high]);
+
+    // Return the partitioning element
+    return (i + 1);
+}
+
+// QuickSort function
+void quickSort(int arr[], int low, int high)
+{
+    // Base case
+    if (low < high)
+    {
+        // Partition the array
+        int pi = partition(arr, low, high);
+
+        // Sort the sub-arrays recursively
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 7, 8, 9, 1, 5};
+
+    // get the array's size
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
