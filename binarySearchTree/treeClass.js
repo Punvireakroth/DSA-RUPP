0a43786f1e71469b441dee917a7e45231032b8e4
Punvireakroth <vireakrothpun@gmail.com>
2023-05-05 15:19:23 +0700

    insert item to the tree

--- binarySearchTree/treeClass.js
+++ binarySearchTree/treeClass.js
@@ -1,20 +1,50 @@
 class Node {
   constructor(value) {
     this.value = value;
-    this.right = null;
     this.left = null;
+    this.right = null;
   }
 }
 
-class binarySearchTree {
+class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(value) {
+    var newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    var current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
 }
 
-let tree = new binarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
-console.log(tree);
+//      10
+//   5     13
+// 2  7  11  16
+
+var tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(11);
+tree.insert(2);
+tree.insert(16);
+tree.insert(7);
